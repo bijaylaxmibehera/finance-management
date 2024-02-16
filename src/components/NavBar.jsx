5bbaf51632fdb817f7b521db4bb8e82fd589b975
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-02-16 14:15:09 +0530

    feat: minor change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,12 +7,16 @@ export const NavBar=()=>{
         color:isActive?"#ffcce6":""
     })
     return (
+        <div>
         <nav className="flex flex-col  gap-4  h-[100%] mx-auto w-[40%]">
             <NavLink to="/" className="hover:font-medium" style={getStyle}>New Entries</NavLink>
             <NavLink to="/income" className="hover:font-medium" style={getStyle}>Income</NavLink>
             <NavLink to="/expenses" className="hover:font-medium" style={getStyle}>Expenses</NavLink>
             <NavLink to="/savings" className="hover:font-medium" style={getStyle}>Savings</NavLink>
             <NavLink to="/dashboard" className="hover:font-medium" style={getStyle}>Dashboard</NavLink>
+            <NavLink to="https://github.com/bijaylaxmibehera/finance-management" target="_blank">Git repo</NavLink>
+            <NavLink to="https://replit.com/@Bijaylaxmi2117/finance-management" target="_blank">API</NavLink>
         </nav>
+        </div>
     )
 }
